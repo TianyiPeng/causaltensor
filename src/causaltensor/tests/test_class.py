b4e9c063a734a31417ea66fe6499b1fe79277c70
Arushi Jain <arushijain1154@gmail.com>
2023-10-20 15:38:41 -0700

    Add test case for dcpr multiple interventions

--- src/causaltensor/tests/test_class.py
+++ src/causaltensor/tests/test_class.py
@@ -4,7 +4,9 @@ from causaltensor.cauest.OLSSyntheticControl import ols_synthetic_control
 from causaltensor.cauest.DID import DID
 from causaltensor.cauest.SDID import SDID
 from causaltensor.cauest.DebiasConvex import DC_PR_auto_rank, DC_PR_with_suggested_rank
-from causaltensor.cauest.MCNNM import MC_NNM_with_cross_validation
+from causaltensor.cauest.MCNNM import MC_NNM_with_cross_validation, MC_NNM_with_suggested_rank
+from causaltensor.matlib import low_rank_M0_normal
+from causaltensor.matlib import iid_treatment
 
 
 np.random.seed(0)
@@ -50,15 +52,13 @@ class TestClass:
         assert M.shape == O.T.shape
         assert tau <= -10 and tau >= -20
 
-    def test_dcpr_auto(self, create_dataset):
+    def test_dcpr(self, create_dataset):
         O, Z = create_dataset
         M, tau, std = DC_PR_auto_rank(O, Z)
         # TODO: Check for better assertions
         assert M.shape == O.shape
         assert tau <= -10 and tau >= -20
 
-    def test_dcpr_rank(self, create_dataset):
-        O, Z = create_dataset
         suggest_r = 2
         M, tau, std = DC_PR_with_suggested_rank(O, Z, suggest_r)
         # TODO: Check for better assertions
@@ -66,12 +66,58 @@ class TestClass:
         assert np.linalg.matrix_rank(M) == suggest_r
         assert tau <= -10 and tau >= -20
 
+
+
+    def test_dcpr_multiple(self):
+        n1 = 100
+        n2 = 100
+        r = 2
+        M0 = low_rank_M0_normal(n1 = n1, n2 = n2, r = r) #low rank baseline matrix
+
+        num_treat = 5 #number of treatments
+        prob = 0.2
+        Z = []
+        tau = []
+        for k in range(num_treat):
+            Z.append(iid_treatment(prob=prob, shape=M0.shape)) #treatment patterns
+            tau.append(np.random.normal(loc=0, scale=1)) #treatment effects
+        
+        def adding_noise(M0, Z, tau, Sigma, SigmaZ):
+            num_treat = len(Z)
+            O = M0 + np.random.normal(loc=0, scale=1, size=M0.shape) * Sigma #add heterogenous noise to the baseline matrix
+            for k in range(num_treat):
+                O += Z[k] * tau[k] + Z[k] * SigmaZ[k] * np.random.normal(loc=0, scale=1, size=M0.shape) #add heterogeneous noise to the treatment effects
+            return O
+        Sigma = np.random.rand(M0.shape[0], M0.shape[1])
+        SigmaZ = []
+        for k in range(num_treat):
+            SigmaZ.append(np.random.rand(M0.shape[0], M0.shape[1]))
+
+        results = []
+        for T in range(1000):
+            O = adding_noise(M0, Z, tau, Sigma, SigmaZ)
+            M, tau_hat, standard_deviation = DC_PR_with_suggested_rank(O, Z, suggest_r=r, method="non-convex") #solving a non-convex optimization to obtain M and tau
+            results.append(tau_hat)
+        results = np.array(results)
+        assert M.shape == O.shape
+        assert np.linalg.norm(np.mean(results, axis=0)-tau) < 0.01
+
+        # TODO: Any checks on SD?
+
+
     def test_mc(self, create_dataset):
         O, Z = create_dataset
         M, a, b, tau = MC_NNM_with_cross_validation(O, 1-Z)
         # TODO: Check for better assertions
         assert M.shape == O.shape
         assert tau <= -15 and tau >= -25
+
+        suggest_r = 2
+        M, a, b, tau = MC_NNM_with_suggested_rank(O, 1-Z, suggest_r)
+        # TODO: Check for better assertions
+        assert M.shape == O.shape
+        assert tau <= -20 and tau >= -30
+        assert np.linalg.matrix_rank(M) == suggest_r
 
 
 
